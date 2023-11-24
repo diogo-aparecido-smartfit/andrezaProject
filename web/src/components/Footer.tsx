8800e053b694216e95265eff6f5e9e1692d9839d
DiogoAMV <diogo.amv19@gmail.com>
2023-11-24 16:33:52 -0300

    feat: login button added

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,6 +1,9 @@
 import { AiFillInstagram, AiOutlineWhatsApp } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { useMyContext } from "./DataContext";
+import { FaFacebookF } from "react-icons/fa";
+import Button from "./Button";
+import { IoLogIn } from "react-icons/io5";
 
 export default function Footer() {
   const navigate = useNavigate();
@@ -12,26 +15,41 @@ export default function Footer() {
   ];
 
   return (
-    <footer className="flex flex-row justify-around gap-7 text-xl text-gray-400 items-center p-12 mt-12">
-      <a
-        href={social[0]}
-        target="_blank"
-        className="hover:text-pink-700 transition-all"
-      >
-        <AiFillInstagram />
-      </a>
+    <footer className="flex flex-row w-full justify-around gap-7 text-xl text-gray-400 items-center p-12 mt-12">
+      <div className="flex gap-7">
+        <a
+          href={social[0]}
+          target="_blank"
+          className="hover:text-pink-700 transition-all"
+        >
+          <AiFillInstagram />
+        </a>
+        <a
+          href={social[1]}
+          target="_blank"
+          className=" hover:text-green-600 transition-all"
+        >
+          <AiOutlineWhatsApp />
+        </a>
+        <a
+          href={social[1]}
+          target="_blank"
+          className=" hover:text-green-600 transition-all"
+        >
+          <FaFacebookF />
+        </a>
+      </div>
 
       <button onClick={() => navigate("/login")} className="text-base">
         © Andreza Moda Praia
       </button>
 
-      <a
-        href={social[1]}
-        target="_blank"
-        className=" hover:text-green-600 transition-all"
+      <Button
+        onClick={() => navigate("/login")}
+        className="text-base bg-black text-white"
       >
-        <AiOutlineWhatsApp />
-      </a>
+        <IoLogIn /> Fazer login
+      </Button>
     </footer>
   );
 }
